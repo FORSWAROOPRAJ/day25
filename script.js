4ae40ef3f4899f0713fd93ac8119892454aef6f1
SWAROOP R <60713891+FORSWAROOPRAJ@users.noreply.github.com>
2022-01-16 12:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+function print() {
+    event.preventDefault();
+
+    var name = (document.querySelector("input").value).toLowerCase();
+    if (name === "japan") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=jp" alt=${name}>`;
+    }
+    else if (name === "pakisthan") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=pk" alt=${name}>`;
+
+    }
+    else if (name === "indonesia") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=id" alt=${name}>`;
+
+    }
+    else if (name === "vietnam") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ve" alt=${name}>`;
+
+    }
+    else if (name === "dr congo") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=co" alt=${name}>`;
+
+    }
+    else if (name === "turkey") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=tr" alt=${name}>`;
+
+    }
+    else if (name === "united kindom" || name === "america" || name === "us" || name === "united state") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=us" alt=${name}>`;
+
+    }
+    else if (name === "myanmar") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=mm" alt=${name}>`;
+
+    }
+    else if (name === "morocco") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ma" alt=${name}>`;
+
+    }
+    else if (name === "sudan") {
+
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=sd" alt=${name}>`;
+
+    }
+    else if (name === "south korea") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=kr" alt=${name}>`;
+    }
+    else if (name === "niger") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ne" alt=${name}>`;
+    }
+    else if (name === "burkina faso") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=bf" alt=${name}>`;
+    }
+    else if (name === "senegal") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=sn" alt=${name}>`;
+    }
+    else if (name === "israel") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=il" alt=${name}>`;
+    }
+    else if (name === "togo") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=tg" alt=${name}>`;
+    }
+    else if (name === "burundi") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=bi" alt=${name}>`;
+    }
+    else if (name === "austria") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=at" alt=${name}>`;
+    }
+    else if (name === "portugal") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=pt" alt=${name}>`;
+    }
+    else if (name === "papua new guinea") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=pg" alt=${name}>`;
+    }
+    else if (name === "haiti") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ht" alt=${name}>`;
+    }
+    else if (name === "tajikistan") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=tj" alt=${name}>`;
+    }
+    else if (name === "honduras") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=hn" alt=${name}>`;
+    }
+    else if (name === "sweden") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=se" alt=${name}>`;
+    }
+    else if (name === "nepal") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=np" alt=${name}>`;
+    }
+    else if (name === "tunisia") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=tn" alt=${name}>`;
+    }
+    else if (name === "guatemala") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=gt" alt=${name}>`;
+    }
+    else if (name === "guinea") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=gn" alt=${name}>`;
+    }
+    else if (name === "netherlands") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=nl" alt=${name}>`;
+    }
+    else if (name === "kazakhstan") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=kz" alt=${name}>`;
+    }
+    else if (name === "tanzania") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=tz" alt=${name}>`;
+    }
+    else if (name === "zambia") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=zm" alt=${name}>`;
+    }
+    else if (name === "mali") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ml" alt=${name}>`;
+    }
+    else if (name === "chile") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=cl" alt=${name}>`;
+    }
+    else if (name === "angola") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ao" alt=${name}>`;
+    }
+    else if (name === "cameroon") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=cm" alt=${name}>`;
+    }
+    else if (name === "malwai") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=mw" alt=${name}>`;
+    }
+    else if (name === "madagascar") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=mg" alt=${name}>`;
+    }
+    else if (name === "mozambique") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=mz" alt=${name}>`;
+    }
+    else if (name === "malaysia") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=my" alt=${name}>`;
+    }
+    else if (name === "ukraine") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=ua" alt=${name}>`;
+    }
+    else if (name === "iraq") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=iq" alt=${name}>`;
+
+    }
+    else if (name === "poland") {
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=pl" alt=${name}>`;
+
+    }
+    else {
+        var arr = name.split("");
+        var val = arr.slice(0, 2);
+        var val1 = val.join("");
+        document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=${val1}" alt=${name}>`;
+    }
+
+}
+function world() {
+    event.preventDefault();
+    document.querySelector(".pic").innerHTML = `<img src="https://corona.dnsforfamily.com/graph.png?c=global">`;
+}
