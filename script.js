54899e4a732c78fe5f150918612d27c46083541b
SWAROOP R <60713891+FORSWAROOPRAJ@users.noreply.github.com>
2022-01-16 12:34:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,100 +1,43 @@
-// URL//
-var url="https://api.openweathermap.org/data/2.5/weather?q=belgaum&lang=en&units=metric&appid=78ea530bcb66434fe3d2276971510974";
 
-// Live time 
-function date_time(time){
-    let unix_timestamp = time;
-// Create a new JavaScript Date object based on the timestamp
-// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-var date = new Date(unix_timestamp * 1000);
-// Hours part from the timestamp
-var hours = date.getHours();
-// Minutes part from the timestamp
-var minutes = "0" + date.getMinutes();
-// Seconds part from the timestamp
-var seconds = "0" + date.getSeconds();
+//A loop to get no of data//
 
-// Will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-return formattedTime;
-}
-//Display Time
-date_time();
-var datetime = new Date();
-document.getElementById("time").textContent = datetime; //it will print on html page
-function refreshTime() {
-    const timeDisplay = document.getElementById("time");
-    const dateString = new Date().toLocaleString();
-    const formattedString = dateString.replace(", ", " - ");
-    timeDisplay.textContent = formattedString;
-  }
-    setInterval(refreshTime, 1000);
-
-//City Search function
-function city(){
-    var city_entered=document.getElementById("search-bar").value;
-    document.getElementById("search-bar").value="";
-    if(city_entered===""){
-        document.getElementById("alert").innerHTML=`<div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>Enter City!</strong> Enter a valid City name.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div> 
-      `;
+function noOfPokemon() {
+    for (i = 1; i <= 50; i++) {
+        getData(i);
     }
-    else{
-       let  customuUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city_entered}&lang=en&units=metric&appid=78ea530bcb66434fe3d2276971510974`;
-        weather(customuUrl);   
-    } 
 }
 
-//Featching Data
-function weather(customuUrl){  
-    fetch(customuUrl)
-    .then((res)=>res.json())
-    .then((result)=>{
-        display(result);        
-    })
-    .catch(()=>{
-      fetch(url)
-      .then((res)=>res.json())
-      .then((result)=>{
-          display(result);        
-      })
-        document.getElementById("alert").innerHTML=`<div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>City not found!</strong> Enter a valid City name.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>`;
-    })
+//To fetch the data//
+
+async function getData(id) {
+    try{
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const detail = await data.json();
+    createData(detail);
+    }catch(error){
+        console.log("Error",error);
+    }
 }
+noOfPokemon();
 
-//Displaying The result
-function display(data){
-    const city_name=document.getElementById("city-name");
-    city_name.innerText=`${data.name}`.toUpperCase();  
-    document.getElementById("temp").innerHTML=`<h1>${data.main.temp}<sup>o<sub>C</sub></sup></h1>`
-    document.getElementById("cloud").innerText=`${data.weather[0].description}`
-    document.getElementById("low").innerHTML=`Low <h4>${data.main.temp_min}<sup>o<sub>C</sub></sup></h4>`;
-    document.getElementById("high").innerHTML=`High <h4>${data.main.temp_max}<sup>o<sub>C</sub></sup></h4>`;
-    document.getElementById("rise").innerHTML=`Sun Rise <h5>${date_time(data.sys.sunrise)}</h5>`;
-    document.getElementById("set").innerHTML=`Sun Set <h5>${date_time(data.sys.sunset)}</h5>`;
-    document.getElementById("humidity").innerText=data.main.humidity;
-    document.getElementById("wind").innerText=data.wind.speed;
-    document.getElementById("pressure").innerText=data.main.pressure;
+//To append the data to display//
+
+function createData(detail) {
+    const info = document.createElement("div");
+    info.setAttribute("class", "infoPoke");
+    const innerHTMLCont = `
+    <div class="pic">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${detail.id}.png"/>
+    </div>
+    <div class="info">
+    <span><b>Name   :${detail.name}</b></span><br>
+    <span><b>Ability:</b>${detail.abilities[0].ability.name}</span><br>
+    <span><b>Move   :</b>${detail.moves[0].move.name}</span><br>
+    <span><b>Weight :</b>${detail.weight}</span>
+    </div>
+    </div>
+    `;
+    info.innerHTML = innerHTMLCont;
+    const container = document.getElementById("container");
+    container.append(info);
 }
-
-//Enter key press eventListner
-var input = document.getElementById("search-bar");
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById("search-btn").click();
-  }
-});
-
-
-
